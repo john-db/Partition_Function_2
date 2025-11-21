0f118198ee3150fd7191f709a88fb2a3262cc57d
John <johnbrdgrs@gmail.com>
2025-11-21 16:46:50 -0500

    Replaced current_indices with a bit vector (called alive) that keeps track of which subtrees are candiates for merges

--- src/sampler_cp.py
+++ src/sampler_cp.py
@@ -6,19 +6,8 @@ from IPython import embed
 import pyarrow as pa
 import pyarrow.parquet as pq
 
-
-#Global constants
-rng = np.random.default_rng(seed=42)
-rng_cp = cp.random.default_rng(seed=42)
-
-eps=0.1
-delta=0.8
-coef=10
-divide=True
-divide_factor=10
-batch_size=100
-
-def main(path, num_samples, alpha, beta, out_path, seed=None):
+def main(path, num_samples, alpha, beta, out_path):
+    print("Running file:", __file__)
 
     start = time.time()
 
@@ -68,11 +57,8 @@ def main(path, num_samples, alpha, beta, out_path, seed=None):
             #call recursive function
             subtrees, prob_sequence, correction = draw_sample_bclt_cp(P_stacked.copy(), init_subtrees_stacked.copy(), dist_stacked.copy(), n_cells)
 
-            embed()
+            #embed()
 
-            for j in range(100):
-                temp = [str(prob_sequence[j] / correction[j]), str(subtrees.shape[0]), "".join(map(lambda x: str(int(x)), subtrees[:,:,j].flatten()))]
-                print((f" ".join(temp)))
 
             #write to parquet
             table = pa.Table.from_arrays([
@@ -81,11 +67,12 @@ def main(path, num_samples, alpha, beta, out_path, seed=None):
                                             pa.FixedSizeListArray.from_arrays(pa.array(subtrees.flatten().get()), bt_size)     #binary trees !!!!!!! BUGGGG USE RESHAPE TO MAKE SURE FORMAT STAYS  subtrees.reshape(-1, subtrees.shape[2])
                                         ], schema=schema)
             writer.write_table(table)
-
             i+= batch_size
+            print(f"Sampled {i} trees so far.")
 
 
-    print(f"Sampled {i} subtrees: " + str(time.time() - start) + " seconds")
+
+    print(f"Sampled {i} trees: " + str(time.time() - start) + " seconds")
 
     cp.cuda.Device(0).synchronize()
 
@@ -102,14 +89,16 @@ def draw_sample_bclt_cp(P, subtrees, dist, n_cells):
         norm_factor: the correction factor (to be used with prior_prob to find the probability of that tree topology being sampled)
     """
 
-    temp_rng = [np.random.default_rng(seed=i) for i in range(batch_size)]
-
-    subtrees, prior_prob, norm_factor = sample_rec_cp(P, subtrees, dist, n_cells, 0, cp.tile(np.arange(n_cells, dtype=int), (batch_size, 1)))
+    alive_row = cp.concatenate([
+        cp.ones(n_cells, dtype=bool),
+        cp.zeros(n_cells - 1, dtype=bool)
+    ])
+    subtrees, prior_prob, norm_factor = sample_rec_cp(P, subtrees, dist, n_cells, 0, cp.tile(alive_row, (batch_size, 1)))
     return subtrees, prior_prob, norm_factor
     #return subtrees[n_cells:-1], prior_prob, norm_factor # the [n_cells:-1] removes the trivial subtrees (singletons) at the beginning and the trivial subtrees (root) at the end
 
 
-def sample_rec_cp(P, subtrees, dist, n_cells, iter, current_indices):
+def sample_rec_cp(P, subtrees, dist, n_cells, iter, alive):
     """
     Recursive function for the bottom-up sampling of trees. The arrays (P, subtrees, dist) have been initialized to have enough space to contain
     all values that they will during the execution of the function, and current_indices keeps track of indices of these arrays
@@ -124,8 +113,8 @@ def sample_rec_cp(P, subtrees, dist, n_cells, iter, current_indices):
     print(iter, end=" ", flush=True)
 
     #base case
-    if current_indices.shape[1] == 1:
-        return subtrees, cp.full(current_indices.shape[0], 1, dtype=np.float64), cp.full(current_indices.shape[0], 1, dtype=np.float64)
+    if cp.sum(alive[0]) == 1:
+        return subtrees, cp.full(batch_size, 1, dtype=np.float64), cp.full(batch_size, 1, dtype=np.float64)
 
 
     # Depending on the number of mutations, the distance values can be quite large in magnitude. This causes the
@@ -152,24 +141,20 @@ def sample_rec_cp(P, subtrees, dist, n_cells, iter, current_indices):
 
 
 
-    # Draw a pair from the distribution, and find which "current_indices" value it corresponds to
 
     #flatten each batch
     prob_flat = prob.reshape(prob.shape[0] * prob.shape[1], prob.shape[2])
 
-    #apply choise batch wise
+    #apply choice batch wise
 
-    ind = cp.apply_along_axis(lambda x: cp.random.choice(len(x), size=1, p=x), axis=0, arr=prob_flat)
-    #### TEMPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP
-    # ind = cp.array([[temp_rng[i].choice(len(prob_flat.get()[:,i]), p=prob_flat.get()[:,i]) for i in range(prob_flat.get().shape[1])]])
-    #### TEMPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP
+    cdf = cp.cumsum(prob_flat, axis=0)
+    u = rng.random(batch_size) # REMEMBER TO REMOVE ASARRAY WHEN YOU SWITCH TO CUPY GENERATOR
+    ind = cp.argmax(u[cp.newaxis, :] <= cdf, axis=0).astype(cp.int32)
 
-    pair = cp.unravel_index(ind, (prob.shape[0],prob.shape[1]))
-    pair = cp.stack([pair[0][0], pair[1][0]]).T
+
+    pair = cp.stack(cp.unravel_index(ind, (prob.shape[0], prob.shape[1])), axis=1)
     pair = cp.sort(pair, axis=1)
 
-    # pair_current = cp.apply_along_axis(lambda x: cp.searchsorted(current_indices, x), axis=0, arr=pair)
-    pair_current = cp.array([cp.searchsorted(current_indices[i], pair[i]) for i in range(current_indices.shape[0])])
 
     # Update the arrays:
     # create a new subtree that is the result of merging the pair of subtrees that were selected
@@ -184,48 +169,29 @@ def sample_rec_cp(P, subtrees, dist, n_cells, iter, current_indices):
     dist[n_cells + iter] -=coef * cp.sum(cp.minimum(P[n_cells + iter], P), axis=1)
     dist[:, n_cells + iter, :] = dist[n_cells + iter, :, :]
 
-    # store the values we are removing from current indices for later
-    removed = (pair[:, 0], pair[:, 1]) #, current_indices[-2])
+    removed = (pair[:, 0], pair[:, 1])
 
-    # shift current indices in order to remove the indices of the pair of selected subtrees
-    # current_indices[pair_current[0]:-1] = current_indices[pair_current[0] + 1:]
-    for i in range(current_indices.shape[0]): #currently limited to for loop due to "ragged indexing" or write custom kernel in the future
-        current_indices[i, pair_current[i, 0]:-1] = current_indices[i, pair_current[i, 0] + 1:]
-        current_indices[i, pair_current[i, 1] - 1:-1] = current_indices[i, pair_current[i, 1]:]
+    alive[batch_array,removed[0]] = 0
+    alive[batch_array,removed[1]] = 0
+    alive[:, n_cells + iter] = 1
+    
+    mask = ~alive
+    batch, idx = cp.nonzero(mask) # returns a tuple of arrays of indices where mask is nonzero. 
 
-    # add a new entry for the new subtree
-    current_indices[:,-2] = n_cells + iter
-
-    # remove the entries in the new row/column that correspond to subtrees that are no longer "current"
-    # dist[n_cells + iter, np.isin(np.arange(dist.shape[1]), current_indices[:-1], assume_unique=True, invert=True)] = np.nan
-
-    for i in range(dist.shape[2]):
-        mask = cp.isin(cp.arange(dist.shape[1]), current_indices[i, :-1], assume_unique=True, invert=True)
-        dist[n_cells + iter, mask, i] = cp.nan
-        dist[mask, n_cells + iter, i] = cp.nan
+    dist[n_cells + iter, idx, batch] = cp.nan
+    dist[idx, n_cells + iter, batch] = cp.nan
 
     dist[n_cells + iter, n_cells + iter,:] = np.nan # remove diagonal entry
-
+    
     # remove the entries corresponding to the selected subtrees from the distance matrix
-    dist[removed[0], :, cp.arange(dist.shape[2])] = np.nan
-    dist[:, removed[0], cp.arange(dist.shape[2])] = np.nan
-    dist[removed[1], :, cp.arange(dist.shape[2])] = np.nan
-    dist[:, removed[1], cp.arange(dist.shape[2])] = np.nan
+    dist[removed[0], :, cp.arange(dist.shape[2])] = cp.nan
+    dist[:, removed[0], cp.arange(dist.shape[2])] = cp.nan
+    dist[removed[1], :, cp.arange(dist.shape[2])] = cp.nan
+    dist[:, removed[1], cp.arange(dist.shape[2])] = cp.nan
 
 
-
-    subtrees, prior_prob, norm_factor = sample_rec_cp(P, subtrees, dist, n_cells, iter + 1, current_indices[:, :-1])
+    subtrees, prior_prob, norm_factor = sample_rec_cp(P, subtrees, dist, n_cells, iter + 1, alive)
     start_full = time.time()
-
-    # Reconstructs the state of current_indices from before the recursive call so we can accumulate probability
-    # of making progress towards the final tree
-    # current_indices[-2] = removed[2] # it works without this line, why?
-    for i in range(dist.shape[2]):
-        current_indices[i, pair_current[i,1] : ] = current_indices[i, pair_current[i,1] - 1 : -1]
-        current_indices[i, pair_current[i,0] + 1 : ] = current_indices[i, pair_current[i,0] : -1]
-
-    current_indices[cp.arange(dist.shape[2]), pair_current[:,0] ] = removed[0]
-    current_indices[cp.arange(dist.shape[2]), pair_current[:,1] ] = removed[1]
 
 
     ################## BEGIN WORKING BUT SLOW ###################
@@ -238,9 +204,8 @@ def sample_rec_cp(P, subtrees, dist, n_cells, iter, current_indices):
         structured_coords = cp.stack((rows, cols), axis=1) #np.array(list(zip(rows, cols)))
 
         def merge_helper(x):
-            pair_i = cp.searchsorted(current_indices[i, :], x)
-            st1 = subtrees[:,:,i][current_indices[i, :][pair_i[0]]]
-            st2 = subtrees[:,:,i][current_indices[i, :][pair_i[1]]]
+            st1 = subtrees[:,:,i][x[0]]
+            st2 = subtrees[:,:,i][x[1]]
 
             return st1+st2
 
@@ -286,19 +251,30 @@ if __name__ == "__main__":
 
     parser.add_argument("-s", "--seed", type=int,
                         help="random seed", required=False, default=None)
-    #parser.add_argument("-c", "--coef", type=float,
-    #                     help="coef", required=False, default=10.0)
-    #parser.add_argument("-e", "--epsilon", type=float,
-    #                     help="epsilon", required=False, default=0.1)
-    #parser.add_argument("-d", "--delta", type=float,
-    #                     help="delta", required=False, default=0.8)
-    # parser.add_argument("-di", "--divide", type=bool,
-    #                     help="If True, use divide normalizaion with parameter divide_factor", required=False, default=False)
-    # parser.add_argument("-df", "--divide_factor", type=float,
-    #                     help="divide_factor", required=False, default=10)
-
+    parser.add_argument("-b", "--batch_size", type=int,
+                        help="batch size", required=False, default=25)
+    parser.add_argument("-c", "--coef", type=float,
+                        help="coef", required=False, default=10.0)
+    parser.add_argument("-e", "--epsilon", type=float,
+                        help="epsilon", required=False, default=0.1)
+    parser.add_argument("-d", "--delta", type=float,
+                        help="delta", required=False, default=0.8)
+    parser.add_argument("-di", "--divide", type=bool,
+                        help="If True, use divide normalizaion with parameter divide_factor", required=False, default=True)
+    parser.add_argument("-df", "--divide_factor", type=float,
+                        help="divide_factor", required=False, default=10)
 
     args = parser.parse_args()
+
+    #Global constants. TODO: These should probably be given as parameters to main instead because it could be confusing
+    rng = cp.random.default_rng(seed=args.seed)
+    eps = args.epsilon
+    delta = args.delta
+    coef = args.coef
+    divide = args.divide
+    divide_factor = args.divide_factor
+    batch_size = args.batch_size
+    batch_array = cp.arange(batch_size)
     # if os.path.exists(args.output):
     #     raise FileExistsError("There is already a file at the output path")
     if args.alpha < 0 or args.alpha > 1:
@@ -307,4 +283,8 @@ if __name__ == "__main__":
         raise Exception("False negative probability must be in the interval [0.0, 1.0]")
     if args.num_samples < 1:
         raise Exception("Number of samples must be a positive integer")
-    main(args.input_matrix, args.num_samples, args.alpha, args.beta, args.output, args.seed)
+    if args.num_samples % batch_size != 0:
+        raise Exception(
+            f"num_samples ({args.num_samples}) must be divisible by batch_size ({batch_size}). batch_size can be set with -b or --batch_size (default 25)"
+        )
+    main(args.input_matrix, args.num_samples, args.alpha, args.beta, args.output)
